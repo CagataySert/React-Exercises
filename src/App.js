58bbea9cba1d8fb429e6c196cada2af24b3f378f
Cagatay Sert <cagatay.sert5@gmail.com>
2019-03-16 02:57:58 +0300

    some codes which are related to props  were added to project. (Section:3)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Navbar from './components/Navbar';
+import User from './components/User';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Merhaba React</h1>
-				<label htmlFor="name">asdasd</label>
+			<div className="container">
+				<Navbar title="User App" />
+				<hr />
+				<User name="Çağatay Sert" salary="5000" department="Bilişim" />
+				<User name="Tarık Sert" salary="6000" department="Bilişim" />
 			</div>
 		);
 	}
