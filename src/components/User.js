bb1c548210435898228c147f60d672f53ede5cbb
Cagatay Sert <cagatay.sert5@gmail.com>
2019-03-17 21:24:12 +0300

    state changing exercises are added to User component.

--- src/components/User.js
+++ src/components/User.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class User extends Component {
+	// constructor(props) {
+	// 	super(props);
+
+	// 	this.state = {
+	// 		isVisible: false
+	// 	};
+	// },
+	// constructor(props) {
+	// 	super(props);
+	// 	this.onClickEvent = this.onClickEvent.bind(this);
+	// }
+	state = {
+		isVisible: false
+	};
+
+	onClickEvent = (e) => {
+		this.setState({
+			isVisible: !this.state.isVisible
+		});
+	}
+
 	render() {
 		const { name, department, salary } = this.props;
+		const { isVisible } = this.state;
+
 		return (
 			<div className="col-md-8 mb-4">
 				<div className="card">
 					<div className="card-header d-flex justify-content-between">
-						<h4 className="d-inline">{name}</h4>
+						<h4 className="d-inline" onClick={this.onClickEvent}>{name}</h4>
 						<i className="far fa-trash-alt" style={{ cursor: 'pointer' }} />
 					</div>
-					<div className="card-body">
-						<p className="card-text">Maaş : {salary}</p>
-						<p className="card-text">department : {department}</p>
-					</div>
+					{
+						isVisible ?
+							<div className="card-body">
+								<p className="card-text">Maaş : {salary}</p>
+								<p className="card-text">department : {department}</p>
+							</div> : null
+					}
 				</div>
 			</div>
 		);
